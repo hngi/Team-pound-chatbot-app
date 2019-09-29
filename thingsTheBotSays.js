fef0312394aece88810c7eb0bb971b7c60737305
Maphorbs <54281214+Maphorbs@users.noreply.github.com>
2019-09-29 06:12:36 +0100

    Update thingsTheBotSays.js

--- thingsTheBotSays.js
+++ thingsTheBotSays.js
@@ -1,10 +1,14 @@
 // botScript is a list of questions the robot will ask the user.
-var person = prompt("Please enter your name", "Harry Potter");
-
-if (person != null) {
-  document.getElementById("demo").innerHTML =
-  "Hello " + person + "! How are you today?";
-}
+var botScript = [
+  What is your name?",
+    "Nice to hear from you",
+    "I am simply an Interactive demo Bot",
+    "I am glad you are having a good time at HNG",
+    "Make sure you bag a job",
+    "This is the last thing I say before I say goodbye"
+  ];
+  
+  var goodbye = "Goodbye! I have to go now.";
   
   
   
