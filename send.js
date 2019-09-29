7f522978e0691b51b01224c842ac22f6b59d6c03
Maphorbs <54281214+Maphorbs@users.noreply.github.com>
2019-09-29 06:20:40 +0100

    Update send.js

--- send.js
+++ send.js
@@ -1,7 +1,13 @@
-var person = prompt("Please enter your name", "Harry Potter");
-
-if (person != null) {
-  document.getElementById("demo").innerHTML =
-  "Hello " + person + "! How are you today?";
-}
+function send(sender, message) {
+    console.log("🗨 " + sender + ": " + message);
+  
+    // Insert the nextMessage into the HTML.
+    chatArea.insertAdjacentHTML("beforeend", "<div id='chat-" + count + "' class='chat-container'><div class='chat-wrapper' id='chat-a-" + count + "'><p id='a-' class='chat-" + sender + "'>" + message + "</p><div class='avatar avatar-" + sender + "'></div></div></div>");
+  
+    document.getElementById('chat-' + count).scrollIntoView();
+  
+    // Count one more message that has been sent.
+    count += 1;
+  
+ }
   
